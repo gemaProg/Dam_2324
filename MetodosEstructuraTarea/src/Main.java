e436837cbe70d5e9dd57de46ea975591b441a312
gema <gema.garciafernandez@educa.madrid.org>
2023-12-13 14:34:09 +0100

    Actualizando

--- MetodosEstructuraTarea/src/Main.java
+++ MetodosEstructuraTarea/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hola 2smr desde 1dam");
+        System.out.println("Estructura posible Tarea");
     }
 }
